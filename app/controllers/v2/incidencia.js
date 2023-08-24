1f785f672b05cb3d9610703a2784d0d54f959257
IvanGMtz <navi.garces24@gmail.com>
2023-08-23 21:33:11 -0500

    feat: :sparkles: Update endpoint incidencia

--- app/controllers/v2/incidencia.js
+++ app/controllers/v2/incidencia.js
@@ -8,3 +8,22 @@ export const getIncidenciasV2 = async (req, res)=>{
     let result = await collection.find().sort({fecha_reporte:1}).toArray();
     res.send(result);
 }
+
+export const deleteIncidenciaV2 = async (req, res) => {
+    if (!req.rateLimit) return;
+    const IncidenciaIdToDelete = Number(req.body.id);
+    if (!IncidenciaIdToDelete) {
+      res.status(400).json({ message: "Incidencia ID not provided" });
+      return;
+    }
+    try {
+      const result = await collection.deleteOne({ id: IncidenciaIdToDelete });
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: "Incidencia deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Incidencia not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error deleting incidencia", error: error.message });
+    }
+}
--- app/routes/incidencia.js
+++ app/routes/incidencia.js
@@ -2,7 +2,7 @@ import express from "express";
 import {limitGet} from "../middlewares/limit.js";
 import passportHelper from '../helpers/passport.js';
 import {getIncidenciasV1, addIncidenciaV1} from "../controllers/v1/incidencia.js"
-import {getIncidenciasV2} from "../controllers/v2/incidencia.js"
+import {getIncidenciasV2, deleteIncidenciaV2} from "../controllers/v2/incidencia.js"
 import routesVersioning  from 'express-routes-versioning';
 const version = routesVersioning();
 
@@ -13,8 +13,13 @@ appIncidencia.get("/", version({
     "^1.0.0": getIncidenciasV1,
     "~2.2.1": getIncidenciasV2
 })); 
+
 appIncidencia.post("/", version({
     "^1.0.0": addIncidenciaV1
+}));
+
+appIncidencia.delete("/:id", version({
+    "~2.2.1": deleteIncidenciaV2
 }));
 
 export default appIncidencia;
