0a9c6ca7c4ff6af666b9234841e58fce744bf507
IvanGMtz <navi.garces24@gmail.com>
2023-08-23 21:32:28 -0500

    feat: :sparkles: Update endpoint area
    Add version 2

---
+++ app/controllers/v2/area.js
@@ -0,0 +1,43 @@
+import {con} from "../../../config/connection/atlas.js";
+import {siguienteId} from "../../helpers/counter.js";
+
+export const addAreaV2 = async (req, res) => {
+  if (!req.rateLimit) return;
+
+  const requiredFields = [
+    { field: "NOMBRE", message: "Area NOMBRE not provided" },
+    { field: "TIPO", message: "Area TIPO not provided" },
+    { field: "COMPUTADORES", message: "Area COMPUTADORES not provided" },
+    { field: "TECLADOS", message: "Area TECLADOS not provided" },
+    { field: "MOUSE", message: "Area MOUSE not provided" },
+    { field: "DIADEMAS", message: "Area DIADEMAS not provided" }
+  ];
+
+  for (const { field, message } of requiredFields) {
+    if (req.body[field] === undefined) {
+      return res.status(400).json({ message });
+    }
+  }
+
+  const {
+    NOMBRE, TIPO, COMPUTADORES, TECLADOS,MOUSE, DIADEMAS
+  } = req.body;
+
+  const newAreaId = await siguienteId("areas");
+  const newArea = {
+    id: newAreaId,
+    nombre: NOMBRE,
+    tipo: TIPO,
+    computadores: COMPUTADORES,
+    teclados: TECLADOS,
+    mouse: MOUSE,
+    diademas: DIADEMAS
+  };
+
+  try {
+    const result = await collection.insertOne(newArea);
+    res.status(201).json({ message: "Area added successfully", insertedId: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ message: "Error adding area", error: error.errInfo.details.schemaRulesNotSatisfied[0].propertiesNotSatisfied[0].description});
+  }
+};
--- app/routes/area.js
+++ app/routes/area.js
@@ -1,7 +1,8 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
 import passportHelper from '../helpers/passport.js';
-import {getAreasV1, addAreaV1} from "../controllers/v1/area.js"
+import {getAreasV1} from "../controllers/v1/area.js"
+import {addAreaV2} from "../controllers/v2/area.js"
 import routesVersioning  from 'express-routes-versioning';
 
 const version = routesVersioning();
@@ -12,7 +13,7 @@ appArea.get("/", version({
     "^1.0.0": getAreasV1
 })); 
 appArea.post("/", version({
-    "^1.0.0": addAreaV1
+    "~2.2.1":addAreaV2
 }));
 
 export default appArea;
