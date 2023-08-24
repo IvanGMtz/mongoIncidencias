40ffb0103c9251d4c079472d35be34478b9ecef6
IvanGMtz <navi.garces24@gmail.com>
2023-08-24 17:09:28 -0500

    perf: :zap: Update endpoints area and trainer
    Implement changes in JWT

--- app/routes/area.js
+++ app/routes/area.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
-import passportHelper from '../helpers/passport.js';
 import {getAreasV1} from "../controllers/v1/area.js"
 import {addAreaV2} from "../controllers/v2/area.js"
 import routesVersioning  from 'express-routes-versioning';
+import {validarToken} from "../middlewares/JWT.js";
 
 const version = routesVersioning();
 const appArea = express.Router();
-appArea.use(limitGet(), passportHelper.authenticate('bearer', { session: false }));
+appArea.use(limitGet(), validarToken);
 
 appArea.get("/", version({
     "^1.0.0": getAreasV1
--- app/routes/trainer.js
+++ app/routes/trainer.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {limitGet} from "../middlewares/limit.js";
-import passportHelper from '../helpers/passport.js';
 import {getTrainersV1} from "../controllers/v1/trainer.js"
 import {getTrainersV2, addTrainerV2, deleteTrainerV2} from "../controllers/v2/trainer.js"
 import routesVersioning  from 'express-routes-versioning';
+import {validarToken} from "../middlewares/JWT.js";
 const version = routesVersioning();
 const appTrainer = express.Router();
 
-appTrainer.use(limitGet(), passportHelper.authenticate('bearer', { session: false }));
+appTrainer.use(limitGet(), validarToken);
 
 appTrainer.get("/", version({
     "^1.0.0": getTrainersV1,
